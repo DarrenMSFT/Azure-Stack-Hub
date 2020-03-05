6acacfa53c71bc69b5684b154c4b438e21116631
DarrenMSFT <34681982+DarrenMSFT@users.noreply.github.com>
2020-03-05 17:37:04 +0000

    Delete aks-engine-k8sclustercreate.ps1

--- aks-engine-k8sclustercreate.ps1
+++
@@ -1,9 +0,0 @@
-﻿choco install aks-engine --version 0.43.0 -y
-
-aks-engine get-versions
-
-cd "C:\Users\dasmall\OneDrive - Microsoft\Azure Stack\DEMOS\Azure Stack Hub\Kubernetes\aks-engine"
-
-curl -o kubernetes-azurestack.json https://raw.githubusercontent.com/Azure/aks-engine/master/examples/azure-stack/kubernetes-azurestack.json
-
-aks-engine deploy  --azure-env AzureStackCloud  --location mas1 --resource-group kube-rg  --api-model ./kubernetes-azurestack.json  --output-directory kube-rg  --client-id XXX  --client-secret XXX  --subscription-id XXX
