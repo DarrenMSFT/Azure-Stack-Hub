bbb3d1d964d58ad59a47331e47773efb05f48aed
DarrenMSFT <34681982+DarrenMSFT@users.noreply.github.com>
2020-03-05 17:32:45 +0000

    Add files via upload

---
+++ aks-engine-k8sclustercreate.ps1
@@ -0,0 +1,9 @@
+﻿choco install aks-engine --version 0.43.0 -y
+
+aks-engine get-versions
+
+cd "C:\Users\dasmall\OneDrive - Microsoft\Azure Stack\DEMOS\Azure Stack Hub\Kubernetes\aks-engine"
+
+curl -o kubernetes-azurestack.json https://raw.githubusercontent.com/Azure/aks-engine/master/examples/azure-stack/kubernetes-azurestack.json
+
+aks-engine deploy  --azure-env AzureStackCloud  --location mas1 --resource-group kube-rg  --api-model ./kubernetes-azurestack.json  --output-directory kube-rg  --client-id 69af8178-d5a1-4da7-9a23-08d0b123fe39  --client-secret VD2YYkI2:2k:w=InthhNg@I45xlWH?Bs  --subscription-id 15a4e3fe-a95b-4dcd-9e5e-e3ac4fae142b
